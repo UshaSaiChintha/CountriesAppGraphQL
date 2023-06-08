7afc7f365c82c1624f626c1adf3b0b354675005e
Usha Sai Chintha <ushasai154@gmail.com>
2023-06-08 11:00:48 +0530

    Initial Commit

---
+++ CountriesAppGraphQL/CountriesAppGraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesAppGraphQLApp.swift
+//  CountriesAppGraphQL
+//
+//  Created by Usha Sai Chintha on 08/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CountriesAppGraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
